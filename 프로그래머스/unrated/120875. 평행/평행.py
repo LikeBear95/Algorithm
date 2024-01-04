0a0d373f483c684c51732b09bf6e542b4a567332
LikeBear95 <139415954+LikeBear95@users.noreply.github.com>
2024-01-04 23:24:47 +0900

    [level unrated] Title: 평행, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/unrated/120875. 평행/평행.py
+++ 프로그래머스/unrated/120875. 평행/평행.py
@@ -1,14 +1,14 @@
 def solution(dots):
     answer = 0
-    line = set()
+    # line = set()  모든 기울기 비교가 아니라 쌍으로 비교
     tmp = []
     for a in range(len(dots)):
         for b in range(a + 1, len(dots)):
             if dots[a][1]-dots[b][1] == 0:
-                line.add(1)
+                # line.add(1)
                 tmp.append(1)
             else:
-                line.add((dots[a][0]-dots[b][0])/(dots[a][1]-dots[b][1]))
+                # line.add((dots[a][0]-dots[b][0])/(dots[a][1]-dots[b][1]))
                 tmp.append((dots[a][0]-dots[b][0])/(dots[a][1]-dots[b][1]))
     if tmp[0] == tmp[5] or tmp[1] == tmp[4] or tmp[2] == tmp[3]:
         answer = 1
