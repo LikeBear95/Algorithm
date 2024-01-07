d00511a027243886330e083fe445bae10189f5c7
LikeBear95 <139415954+LikeBear95@users.noreply.github.com>
2024-01-07 16:06:28 +0900

    [level unrated] Title: 문자열 밀기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120921. 문자열 밀기/문자열 밀기.py
+++ 프로그래머스/unrated/120921. 문자열 밀기/문자열 밀기.py
@@ -1,9 +1,12 @@
 def solution(A, B):
-    for s in range(len(A)-1, 0, -1):
-        if A[s:] + A[:s] == B:
-            answer = len(A) - s
-            break
-        else:
-            answer = -1
-    if A == B: answer = 0
+    # for s in range(len(A)-1, 0, -1):
+    #     if A[s:] + A[:s] == B:
+    #         answer = len(A) - s
+    #         break
+    #     else:
+    #         answer = -1
+    # if A == B: answer = 0
+    
+    AA = A[::-1] + A[::-1]
+    answer = AA.find(B[::-1])
     return answer
