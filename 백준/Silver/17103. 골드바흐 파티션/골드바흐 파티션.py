a561a168bf6a7efe37f199439437c60406b68e3e
LikeBear95 <139415954+LikeBear95@users.noreply.github.com>
2024-06-01 22:33:28 +0900

    Update 골드바흐 파티션.py

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.py
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.py
@@ -24,3 +24,27 @@ for _ in range(T):
         if not check[N - i] and i <= N-i:  # 순서만 다른거 counting 하지 않기 위해
             count += 1
     print(count)
+
+/*
+def sieve_of_eratosthenes(limit):
+    is_prime = [True] * (limit + 1)
+    p = 2
+    while (p * p <= limit):
+        if is_prime[p]:
+            for i in range(p * p, limit + 1, p):
+                is_prime[i] = False
+        p += 1
+    return is_prime
+
+is_prime = sieve_of_eratosthenes(1000000)
+
+for _ in range(int(input())):
+    n = int(input())
+    cnt = 0
+
+    for i in range(2, n // 2 + 1):
+        if is_prime[i] and is_prime[n - i]:
+            cnt += 1
+
+    print(cnt)
+*/
