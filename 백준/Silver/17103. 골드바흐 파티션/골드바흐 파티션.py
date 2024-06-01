b611be87a9bd37ec52ed6dd991c74e8e04fabe0d
LikeBear95 <139415954+LikeBear95@users.noreply.github.com>
2024-06-01 22:31:30 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 1104 ms, Memory: 41600 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.py
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.py
@@ -1,21 +1,26 @@
-def sieve_of_eratosthenes(limit):
-    is_prime = [True] * (limit + 1)
-    p = 2
-    while (p * p <= limit):
-        if is_prime[p]:
-            for i in range(p * p, limit + 1, p):
-                is_prime[i] = False
-        p += 1
-    return is_prime
+# 다른사람꺼
 
-is_prime = sieve_of_eratosthenes(1000000)
+import sys
 
-for _ in range(int(input())):
-    n = int(input())
-    cnt = 0
+prime = []
+check = [0] * 1000001
+check[0] = 1
+check[1] = 1
 
-    for i in range(2, n // 2 + 1):
-        if is_prime[i] and is_prime[n - i]:
-            cnt += 1
+for i in range(2, 1000001):
+    if check[i] == 0:
+        prime.append(i)
+        for j in range(2*i, 1000001, i):
+            check[j] = 1
 
-    print(cnt)
+T = int(sys.stdin.readline())
+
+for _ in range(T):
+    count = 0
+    N = int(sys.stdin.readline())
+    for i in prime:
+        if i >= N:
+            break
+        if not check[N - i] and i <= N-i:  # 순서만 다른거 counting 하지 않기 위해
+            count += 1
+    print(count)
