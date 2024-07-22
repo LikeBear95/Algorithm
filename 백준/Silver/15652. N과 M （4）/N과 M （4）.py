2ac736b5f9d6d7ca9ee09b8c410f7ceed4573186
LikeBear95 <139415954+LikeBear95@users.noreply.github.com>
2024-07-22 22:36:12 +0900

    [Silver III] Title: N과 M (4), Time: 56 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,20 @@
+import sys
+
+def dfs(lst):
+    if len(lst) == m:
+        print(*lst)
+    else:
+        for i in num:
+            if not lst:
+                lst.append(i)
+                dfs(lst)
+                lst.pop()
+            elif i >= lst[-1]:
+                lst.append(i)
+                dfs(lst)
+                lst.pop()
+
+
+n, m = map(int, input().split())
+num = [x for x in range(1, n+1)]
+dfs([])
