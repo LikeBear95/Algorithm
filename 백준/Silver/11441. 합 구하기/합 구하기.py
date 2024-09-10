1b9d25e5be7044b52d2f2ac01ceb1270aeaabc8c
LikeBear95 <139415954+LikeBear95@users.noreply.github.com>
2024-09-10 19:09:24 +0900

    [Silver III] Title: 합 구하기, Time: 152 ms, Memory: 121548 KB -BaekjoonHub

--- 백준/Silver/11441. 합 구하기/합 구하기.py
+++ 백준/Silver/11441. 합 구하기/합 구하기.py
@@ -4,6 +4,10 @@ input = sys.stdin.readline
 n = int(input())
 lst = list(map(int, input().split()))
 
+sum_lst = [0] * (n+1)
+for i in range(1, n+1):
+    sum_lst[i] = sum_lst[i-1] + lst[i-1]
+
 for i in range(int(input())):
     s, e = map(int, input().split())
-    print(sum(lst[s-1:e]))
+    print(sum_lst[e] - sum_lst[s-1])
