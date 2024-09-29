2679e041d999414181aa36306f5d036a346c0f8b
LikeBear95 <139415954+LikeBear95@users.noreply.github.com>
2024-09-29 21:39:00 +0900

    [Bronze III] Title: TGN, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/5063. TGN/TGN.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    r, e, c = map(int, input().split())
+    if r < e - c:
+        print("advertise")
+    elif r > e - c:
+        print("do not advertise")
+    else:
+        print("does not matter")
