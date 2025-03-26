afe8231f24422dfaf0bf5c127f5cc771fcdb4f79
LikeBear95 <139415954+LikeBear95@users.noreply.github.com>
2025-03-26 20:09:57 +0900

    [Silver IV] Title: 암기왕, Time: 948 ms, Memory: 304520 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.py
+++ 백준/Silver/2776. 암기왕/암기왕.py
@@ -1,7 +1,11 @@
+import sys
+input = sys.stdin.readline
+
 for _ in range(int(input())):
     n = int(input())
     st = set(map(int, input().split()))
     m = int(input())
     lst = list(map(int, input().split()))
+    
     for i in lst:
         print(1 if i in st else 0)
