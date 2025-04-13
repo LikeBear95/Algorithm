6c0a74463355ff65fb6b7855728980c75f2bf7e8
LikeBear95 <139415954+LikeBear95@users.noreply.github.com>
2025-04-13 16:56:23 +0900

    [Bronze I] Title: 제자리, Time: 36 ms, Memory: 32412 KB, Score: 49 point -BaekjoonHub

---
+++ 백준/Bronze/25400. 제자리/제자리.py
@@ -0,0 +1,12 @@
+n = int(input())
+lst = list(map(int, input().split()))
+
+for i in range(1, n+1):
+    if i in lst:
+        lst = lst[lst.index(i):]
+        n -= 1
+    else:
+        break
+
+print(n)
+
