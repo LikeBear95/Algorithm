effc3a5c98db0809b0225d4c52535f0267e53d30
LikeBear95 <139415954+LikeBear95@users.noreply.github.com>
2025-05-25 17:47:37 +0900

    [Bronze III] Title: 일우는 야바위꾼, Time: 124 ms, Memory: 33972 KB -BaekjoonHub

--- 백준/Bronze/20361. 일우는 야바위꾼/일우는 야바위꾼.py
+++ 백준/Bronze/20361. 일우는 야바위꾼/일우는 야바위꾼.py
@@ -1,9 +1,13 @@
+import sys
+input = sys.stdin.readline
+
 n, x, k = map(int, input().split())
 l = [0] * (n+1)
 l[x] = 1
 
 for _ in range(k):
     a, b = map(int, input().split())
-    l[a], l[b] = l[b], l[a]
+    if l[a] or l[b]:
+        l[a], l[b] = l[b], l[a]
     
 print(l.index(1))
