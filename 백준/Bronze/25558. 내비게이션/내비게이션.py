349de037ed0e03d105e1ed1cc8789c15242d3bb6
LikeBear95 <139415954+LikeBear95@users.noreply.github.com>
2025-07-26 20:46:32 +0900

    [Bronze II] Title: 내비게이션, Time: 2644 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/25558. 내비게이션/내비게이션.py
@@ -0,0 +1,15 @@
+n = int(input())
+sx, sy, ex, ey = map(int, input().split())
+l = []
+for _ in range(n):
+    m = int(input())
+    t = 0
+    tx, ty = sx, sy
+    for _ in range(m):
+        x, y = map(int, input().split())
+        t += abs(tx-x)+abs(ty-y)
+        tx, ty = x, y
+    t += abs(tx-ex)+abs(ty-ey) 
+    l.append(t)
+
+print(l.index((min(l)))+1)
