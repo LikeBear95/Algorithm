ba6b2c789a04feabbd7aa89db097e018b232171f
LikeBear95 <139415954+LikeBear95@users.noreply.github.com>
2025-08-18 19:18:32 +0900

    [Bronze III] Title: 꿍의 우주여행, Time: 152 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9501. 꿍의 우주여행/꿍의 우주여행.py
@@ -0,0 +1,7 @@
+for i in range(int(input())):
+    n, d = map(int, input().split())
+    t = 0
+    for j in range(n):
+        v, f, c = map(int, input().split())
+        t += 1 if v*f/c >= d else 0
+    print(t)
