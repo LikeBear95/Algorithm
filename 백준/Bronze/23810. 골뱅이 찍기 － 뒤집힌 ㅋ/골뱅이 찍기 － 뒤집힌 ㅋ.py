69fd6dc04f94b4b97f29d319c4737211f315fa45
LikeBear95 <139415954+LikeBear95@users.noreply.github.com>
2025-10-25 01:35:05 +0900

    [Bronze III] Title: 골뱅이 찍기 - 뒤집힌 ㅋ, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/23810. 골뱅이 찍기 － 뒤집힌 ㅋ/골뱅이 찍기 － 뒤집힌 ㅋ.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+for i in range(n):
+    print(f"{'@'*5*n}")
+for i in range(n):
+    print(f"{'@'*n}")
+for i in range(n):
+    print(f"{'@'*5*n}")
+for i in range(2*n):
+    print(f"{'@'*n}")
