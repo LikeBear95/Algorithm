ee724ec4c1cdd4c7ae4b0f99869b910f44806fda
LikeBear95 <139415954+LikeBear95@users.noreply.github.com>
2025-11-01 23:48:04 +0900

    [Bronze III] Title: K 2K 게임, Time: 96 ms, Memory: 37424 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/21603. K 2K 게임/K 2K 게임.py
+++ 백준/Bronze/21603. K 2K 게임/K 2K 게임.py
@@ -2,13 +2,7 @@ def f(x):
     return x%10
 
 n, k = map(int, input().split())
-l = [x for x in range(1, n+1)]
-
-for i in range(f(k), n+1, 10):
-    l.remove(i)
-
-for i in range(f(2*k), n+1, 10):
-    l.remove(i)
+l = [x for x in range(1, n+1) if f(x)!=f(k) and f(x)!=f(2*k)]
 
 print(len(l))
 print(*l if l else "")
