ea200e74034eb288379e36afb045ca2be29c9347
LikeBear95 <139415954+LikeBear95@users.noreply.github.com>
2025-11-16 03:56:26 +0900

    [Bronze II] Title: 폭죽쇼, Time: 1876 ms, Memory: 253912 KB -BaekjoonHub

---
+++ 백준/Bronze/1773. 폭죽쇼/폭죽쇼.py
@@ -0,0 +1,11 @@
+n, c = map(int, input().split())
+s = set()
+
+for _ in range(n):
+    t = int(input())
+    if t in s:
+        pass
+    for i in range(t,c+1,t):
+        s.add(i)
+
+print(len(s))
