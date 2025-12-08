beed979198b82d01944bb5025ca75119ca9df4c2
LikeBear95 <139415954+LikeBear95@users.noreply.github.com>
2025-12-08 18:54:26 +0900

    [Bronze III] Title: 등장하지 않는 문자의 합, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/3059. 등장하지 않는 문자의 합/등장하지 않는 문자의 합.py
+++ 백준/Bronze/3059. 등장하지 않는 문자의 합/등장하지 않는 문자의 합.py
@@ -1,14 +1,8 @@
 import sys
 input = sys.stdin.readline
 
+n = 2015
+
 for _ in range(int(input())):
-    t = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    s = input().rstrip()
-    n = 2015
-
-    for i in s:
-        if i in t:
-            t = t.replace(i, "")
-            n -= ord(i)
-
-    print(n)
+    s = set(input().rstrip())
+    print(n - sum(ord(c) for c in s))
