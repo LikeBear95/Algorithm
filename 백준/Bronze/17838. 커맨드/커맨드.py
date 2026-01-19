cf72e3b665ed7c5cd4267255c7c003a73360673e
LikeBear95 <139415954+LikeBear95@users.noreply.github.com>
2026-01-19 22:11:27 +0900

    [Bronze II] Title: 커맨드, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/17838. 커맨드/커맨드.py
+++ 백준/Bronze/17838. 커맨드/커맨드.py
@@ -3,15 +3,12 @@ input = sys.stdin.readline
 
 for _ in range(int(input())):
     t = input().rstrip()
-    f = 0
-    for i in range(len(t)-6):
-        s = t[i:i+7]
-        if (
-            len(t) == 7 and
-            len(set(t)) == 2 and
-            t[0] == t[1] == t[4] and
-            t[2] == t[3] == t[5] == t[6]
-        ):
-            f = 1
-            break
-    print(f)
+    if (
+        len(t) == 7 and
+        len(set(t)) == 2 and
+        t[0] == t[1] == t[4] and
+        t[2] == t[3] == t[5] == t[6]
+    ):
+        print(1)
+    else:
+        print(0)
