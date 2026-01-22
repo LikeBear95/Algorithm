16b2639faaee723ad9ecd09e0bad8512cee8148a
LikeBear95 <139415954+LikeBear95@users.noreply.github.com>
2026-01-22 16:11:37 +0900

    [Bronze II] Title: 친구, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10864. 친구/친구.py
@@ -0,0 +1,10 @@
+n, m = map(int, input().split())
+l = [[] for _ in range(n)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    l[a-1].append(b-1)
+    l[b-1].append(a-1)
+
+for i in l:
+    print(len(i))
