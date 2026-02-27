fa2ac223c8a6e5c9ae3467109cde3d0cc00d4d27
LikeBear95 <139415954+LikeBear95@users.noreply.github.com>
2026-02-27 19:19:50 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.py
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.py
@@ -1,31 +1,34 @@
 import sys
 input = sys.stdin.readline
 
+v = set('aeiou')
+
 while True:
     s = input().rstrip()
     if s == "end":
         break
-        
-    a, b = "", ""
-    c = 0
+    
+    p = ""
+    a, b, c = 0, 0, 0
     
     for i in s:
-        if i in ['a','e','i','o','u']:
-            a += i
-            b = ""
+        if i in v:
+            a += 1
+            b = 0
             c += 1
         else:
-            a = ""
-            b += i
+            a = 0
+            b += 1
 
-        if len(a)+len(b) >= 3:
+        if a >= 3 or b >= 3:
             print(f'<{s}> is not acceptable.')
             break
 
-        if i not in ['e', 'o'] and a.count(i)+b.count(i) >= 2:
+        if p == i and p not in ['e', 'o']:
             print(f'<{s}> is not acceptable.')
             break
+
+        p = i
 
     else:
         print(f'<{s}> is {"" if c else "not "}acceptable.')
-
