8a093bb09b0661f4649c876852ed50fc5297c764
LikeBear95 <139415954+LikeBear95@users.noreply.github.com>
2026-03-03 18:06:35 +0900

    [Bronze III] Title: 지금부터 완장 찬양을 시작하겠습니다, Time: 44 ms, Memory: 34368 KB -BaekjoonHub

--- 백준/Bronze/35262. 지금부터 완장 찬양을 시작하겠습니다/지금부터 완장 찬양을 시작하겠습니다.py
+++ 백준/Bronze/35262. 지금부터 완장 찬양을 시작하겠습니다/지금부터 완장 찬양을 시작하겠습니다.py
@@ -1,12 +1,3 @@
 n, k = map(int, input().split())
 s = input()
-m, c = 0, 0
-
-for i in range(n):
-    if s[i] == "0":
-        c += 1
-    else:
-        c = 0
-    m = max(c, m)
-
-print(0 if m>=k else 1)
+print(0 if '0'*k in s else 1)
