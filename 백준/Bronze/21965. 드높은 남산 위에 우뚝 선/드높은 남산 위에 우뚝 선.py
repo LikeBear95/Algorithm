a00313848b2639a9b0b0ecc0fe25639e35fcfcaa
LikeBear95 <139415954+LikeBear95@users.noreply.github.com>
2026-03-12 19:57:48 +0900

    [Bronze I] Title: 드높은 남산 위에 우뚝 선, Time: 80 ms, Memory: 42660 KB -BaekjoonHub

--- 백준/Bronze/21965. 드높은 남산 위에 우뚝 선/드높은 남산 위에 우뚝 선.py
+++ 백준/Bronze/21965. 드높은 남산 위에 우뚝 선/드높은 남산 위에 우뚝 선.py
@@ -1,12 +1,13 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 l = list(map(int, input().split()))
+
 m = l.index(max(l))
 
 for i in range(1, n):
-    if i < m and l[i] <= l[i-1]:
-        print("NO")
-        break
-    elif i > m and l[i] >= l[i-1]:
+    if (i <= m and l[i] <= l[i-1]) or (i > m and l[i] >= l[i-1]):
         print("NO")
         break
 else:
