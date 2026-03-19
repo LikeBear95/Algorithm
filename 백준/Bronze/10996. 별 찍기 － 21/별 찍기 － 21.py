f24c224c5f1c9974430804874eebd04e989b534f
LikeBear95 <139415954+LikeBear95@users.noreply.github.com>
2026-03-19 20:37:23 +0900

    [Bronze II] Title: 별 찍기 - 21, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10996. 별 찍기 － 21/별 찍기 － 21.py
+++ 백준/Bronze/10996. 별 찍기 － 21/별 찍기 － 21.py
@@ -1,6 +1,7 @@
-o = ' *'*50
-e = '* '*50
-
 n = int(input())
-for i in range(n*2):
-    print(o[:n] if i%2 else e[:n])
+
+o = (' *' * n)[:n]
+e = ('* ' * n)[:n]
+
+for i in range(n * 2):
+    print(o if i % 2 else e)
