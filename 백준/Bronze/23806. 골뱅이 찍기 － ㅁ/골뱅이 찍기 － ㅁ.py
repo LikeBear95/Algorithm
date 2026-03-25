34e3008486773246799b134211bef6c3461bfe55
LikeBear95 <139415954+LikeBear95@users.noreply.github.com>
2026-03-25 21:55:13 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅁ, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/23806. 골뱅이 찍기 － ㅁ/골뱅이 찍기 － ㅁ.py
@@ -0,0 +1,8 @@
+n = int(input())
+
+for _ in range(n):
+    print(f'{"@"*n}{"@@@"*n}{"@"*n}')
+for _ in range(n*3):
+    print(f'{"@"*n}{"   "*n}{"@"*n}')
+for _ in range(n):
+    print(f'{"@"*n}{"@@@"*n}{"@"*n}')
