de57b91fe205c23e5bf648039d27d721beb11b72
LikeBear95 <139415954+LikeBear95@users.noreply.github.com>
2026-04-07 16:16:39 +0900

    [Bronze III] Title: 친구 친구, Time: 768 ms, Memory: 33908 KB -BaekjoonHub

--- 백준/Bronze/10865. 친구 친구/친구 친구.py
+++ 백준/Bronze/10865. 친구 친구/친구 친구.py
@@ -2,12 +2,12 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-l = [[] for _ in range(n+1)]
+l = [0 for _ in range(n+1)]
 
 for _ in range(m):
     a, b = map(int, input().split())
-    l[a].append(b)
-    l[b].append(a)
+    l[a] += 1
+    l[b] += 1
 
 for i in l[1:]:
-    print(len(i))
+    print(i)
